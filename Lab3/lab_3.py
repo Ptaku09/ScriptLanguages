5fe287e9efe1319c25405ee47ca559cef990003f
Mateusz Ptak <ptakm02@gmail.com>
2023-03-25 07:59:39 +0100

    lab3 - ex3 - log_to_dict

--- Lab3/lab_3.py
+++ Lab3/lab_3.py
@@ -1,11 +1,26 @@
 # entry_to_dict ---------------------
-def entry_to_dict(log):
+
+
+def entry_to_dict(entry):
     return {
-        "ip": log[0],
-        "datetime": log[1],
-        "method": log[2],
-        "path": log[3],
-        "protocol": log[4],
-        "status code": log[5],
-        "size": log[6]
+        "ip": entry[0],
+        "datetime": entry[1],
+        "method": entry[2],
+        "path": entry[3],
+        "protocol": entry[4],
+        "status code": entry[5],
+        "size": entry[6]
     }
+
+
+# log_to_dict -----------------------
+def log_to_dict(log):
+    dictionary = {}
+
+    for e in log:
+        if e[0] not in dictionary:
+            dictionary[e[0]] = [entry_to_dict(e)]
+        else:
+            dictionary[e[0]].append(entry_to_dict(e))
+
+    return dictionary
