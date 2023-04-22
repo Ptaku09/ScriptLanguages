0b93dcd804d86d89e9e17c2171254add75e61fd1
Mateusz Ptak <ptakm02@gmail.com>
2023-04-22 23:00:25 +0200

    lab5 - ex4 - least and most active user

--- Lab5/lab_5_4.py
+++ Lab5/lab_5_4.py
@@ -1,7 +1,7 @@
 import random
 
 from lab_5_1 import read_ssh_logs, parse_ssh_logs
-from lab_5_2 import get_user_from_log
+from lab_5_2 import get_user_from_log, get_message_type, MessageType
 
 
 def group_logs_by_user(logs):
@@ -32,11 +32,49 @@ def get_random_sample_from_random_user_logs(logs, n):
     return random_sample
 
 
+def least_and_most_active_user(logs):
+    grouped_logs = group_logs_by_user(logs)
+    max_success_logins = 0
+    min_success_logins = float('inf')
+    max_users = []
+    min_users = []
+
+    for key, value in grouped_logs.items():
+        user_success_logging = 0
+
+        for log in value:
+            message_type = get_message_type(log['message'])
+            if message_type == MessageType.SUCCESSFUL_LOGGING.value:
+                user_success_logging += 1
+
+        if user_success_logging == max_success_logins:
+            max_users.append(key)
+        elif user_success_logging > max_success_logins:
+            max_users.clear()
+            max_users.append(key)
+            max_success_logins = user_success_logging
+
+        if user_success_logging == min_success_logins:
+            min_users.append(key)
+        elif user_success_logging < min_success_logins:
+            min_users.clear()
+            min_users.append(key)
+            min_success_logins = user_success_logging
+
+    print('Max success logins: ' + str(max_success_logins))
+    print('Max success logins users: ' + str(max_users))
+    print('------------------------')
+    print('Min success logins: ' + str(min_success_logins))
+    print('Min success logins users: ' + str(min_users))
+
+
 if __name__ == '__main__':
-    lines = read_ssh_logs('/Users/mateusz/Desktop/Studia/Semestr IV/[L] Języki skrytpowe/Lab5/test.log')
+    lines = read_ssh_logs('/Users/mateusz/Desktop/Studia/Semestr IV/[L] Języki skrytpowe/Lab5/SSH.log')
     parsed = parse_ssh_logs(lines)
 
     random_logs = get_random_sample_from_random_user_logs(parsed, 3)
 
     for lo in random_logs:
         print(lo)
+
+    least_and_most_active_user(parsed)
