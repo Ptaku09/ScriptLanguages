a35c0de06eaf5a8989a70d2c1c4f0696c9d76e30
Mateusz Ptak <ptakm02@gmail.com>
2023-04-29 13:27:43 +0200

    lab6 - ex7 - magic methods

--- Lab6/SSHLogJournal.py
+++ Lab6/SSHLogJournal.py
@@ -1,3 +1,4 @@
+import datetime
 import ipaddress
 import re
 
@@ -19,3 +20,20 @@ class SSHLogJournal:
         ip = ipaddress.IPv4Address(ip)
 
         return [log for log in self.logs if log.ip == ip]
+
+    def get_logs_by_date(self, date):
+        if re.search(r'^\d{2}-\d{2}-\d{4}$', date) is None:
+            raise ValueError("Invalid date")
+
+        date = datetime.datetime.strptime(date, "%d-%m-%Y").date()
+
+        return [log for log in self.logs if datetime.datetime.strptime(log.date, "%d-%m-%Y") == date]
+
+    def __len__(self):
+        return len(self.logs)
+
+    def __iter__(self):
+        return iter(self.logs)
+
+    def __contains__(self, item):
+        return item in self.logs
