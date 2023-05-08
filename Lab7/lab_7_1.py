d37e32baf66e6bb59be0cbf5ada269c163e01fa6
Mateusz Ptak <ptakm02@gmail.com>
2023-05-08 23:29:49 +0200

    lab7 - ex1 - median

--- Lab7/lab_7_1.py
+++ Lab7/lab_7_1.py
@@ -3,3 +3,12 @@ import string
 
 def acronym(words: list[string]) -> string:
     return "".join(["" if len(word[0]) == 0 else word[0] for word in words]).upper()
+
+
+def median(numbers: list[int]) -> float:
+    numbers.sort()
+
+    if len(numbers) % 2 == 0:
+        return (numbers[len(numbers) // 2 - 1] + numbers[len(numbers) // 2]) / 2
+    else:
+        return numbers[len(numbers) // 2]
