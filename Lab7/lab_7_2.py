0fc9958e5903464c47a37123b52c74b5c0316bad
Mateusz Ptak <ptakm02@gmail.com>
2023-05-10 00:16:33 +0200

    lab7 - ex2 - exists

--- Lab7/lab_7_2.py
+++ Lab7/lab_7_2.py
@@ -4,3 +4,12 @@ def forall(pred, iterable):
     except TypeError:
         print("you should provide a predicate and an iterable")
         exit(1)
+
+
+def exists(pred, iterable):
+    try:
+        return any(pred(x) for x in iterable)
+    except TypeError:
+        print("you should provide a predicate and an iterable")
+        exit(1)
+       
