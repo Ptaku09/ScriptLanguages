ba3bc42ffb0c301e640326b936c9c6f151ac5ef8
Mateusz Ptak <ptakm02@gmail.com>
2023-05-25 11:57:43 +0200

    lab9 - ex1 - add types to SSHLogType

--- Lab9/SSHLogType.py
+++ Lab9/SSHLogType.py
@@ -1,44 +1,45 @@
 from abc import ABC
+from typing import Optional
 
 from SSHLogEntry import SSHLogEntry
 from utils import get_user_from_log, get_port_from_log, MessageType, get_message_type
 
 
 class SSHLogFailedPassword(SSHLogEntry, ABC):
-    def __init__(self, line):
+    def __init__(self, line: str) -> None:
         super().__init__(line)
-        self.user = get_user_from_log(self.message)
-        self.ip = self.get_ipv4()
-        self.port = get_port_from_log(self.message)
+        self.user: str = get_user_from_log(self.message)
+        self.ip: Optional[str] = self.get_ipv4()
+        self.port: Optional[int] = get_port_from_log(self.message)
 
-    def validate(self):
+    def validate(self) -> bool:
         return get_message_type(self.message) == MessageType.FAILED_PASSWORD
 
 
 class SSHLogAcceptedPassword(SSHLogEntry, ABC):
-    def __init__(self, line):
+    def __init__(self, line: str) -> None:
         super().__init__(line)
-        self.user = get_user_from_log(self.message)
-        self.ip = self.get_ipv4()
-        self.port = get_port_from_log(self.message)
+        self.user: str = get_user_from_log(self.message)
+        self.ip: Optional[str] = self.get_ipv4()
+        self.port: Optional[int] = get_port_from_log(self.message)
 
-    def validate(self):
+    def validate(self) -> bool:
         return get_message_type(self.message) == MessageType.ACCEPTED_PASSWORD
 
 
 class SSHLogError(SSHLogEntry, ABC):
-    def __init__(self, line):
+    def __init__(self, line: str) -> None:
         super().__init__(line)
-        self.ip = self.get_ipv4()
+        self.ip: Optional[str] = self.get_ipv4()
 
-    def validate(self):
+    def validate(self) -> bool:
         return get_message_type(self.message) == MessageType.ERROR
 
 
 class SSHLogOther(SSHLogEntry, ABC):
-    def __init__(self, line):
+    def __init__(self, line: str) -> None:
         super().__init__(line)
-        self.ip = self.get_ipv4()
+        self.ip: Optional[str] = self.get_ipv4()
 
-    def validate(self):
+    def validate(self) -> bool:
         return True
