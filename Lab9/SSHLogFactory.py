090af05d53117fa84e629ca8b5e0f1ea915ef51c
Mateusz Ptak <ptakm02@gmail.com>
2023-05-25 12:03:18 +0200

    lab9 - ex1 - add types to SSHLogFactory

--- Lab9/SSHLogFactory.py
+++ Lab9/SSHLogFactory.py
@@ -6,44 +6,44 @@ from SSHLogType import SSHLogFailedPassword, SSHLogAcceptedPassword, SSHLogError
 
 class SSHLogCreator(abc.ABC):
     @abc.abstractmethod
-    def create_log(self, line):
+    def create_log(self, line: str) -> SSHLogEntry:
         pass
 
-    def validate(self, line):
-        log = self.create_log(line)
+    def validate(self, line: str) -> bool:
+        log: SSHLogEntry = self.create_log(line)
         return log.validate()
 
 
 class SSHLogFailedPasswordCreator(SSHLogCreator):
-    def create_log(self, line) -> SSHLogEntry:
+    def create_log(self, line: str) -> SSHLogFailedPassword:
         return SSHLogFailedPassword(line)
 
 
 class SSHLogAcceptedPasswordCreator(SSHLogCreator):
-    def create_log(self, line) -> SSHLogEntry:
+    def create_log(self, line: str) -> SSHLogAcceptedPassword:
         return SSHLogAcceptedPassword(line)
 
 
 class SSHLogErrorCreator(SSHLogCreator):
-    def create_log(self, line) -> SSHLogEntry:
+    def create_log(self, line: str) -> SSHLogError:
         return SSHLogError(line)
 
 
 class SSHLogOtherCreator(SSHLogCreator):
-    def create_log(self, line) -> SSHLogEntry:
+    def create_log(self, line: str) -> SSHLogOther:
         return SSHLogOther(line)
 
 
 class SSHLogFactory:
-    def __init__(self):
-        self._creators = [
+    def __init__(self) -> None:
+        self._creators: list[SSHLogCreator] = [
             SSHLogFailedPasswordCreator(),
             SSHLogAcceptedPasswordCreator(),
             SSHLogErrorCreator(),
             SSHLogOtherCreator()
         ]
 
-    def create_log(self, line):
+    def create_log(self, line: str) -> SSHLogEntry:
         for creator in self._creators:
             if creator.validate(line):
                 return creator.create_log(line)
@@ -51,4 +51,4 @@ class SSHLogFactory:
         raise ValueError("Log type not found")
 
 
-log_factory = SSHLogFactory()
+log_factory: SSHLogFactory = SSHLogFactory()
