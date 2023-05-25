ce369667f7257174355901439a5db6beba807b7e
Mateusz Ptak <ptakm02@gmail.com>
2023-05-25 11:48:35 +0200

    lab9 - ex1 - add types to utils

--- Lab9/utils.py
+++ Lab9/utils.py
@@ -1,34 +1,35 @@
 import re
 from enum import Enum
+from typing import Match, Optional
 
 
-def is_line_valid(line):
+def is_line_valid(line: str) -> Optional[Match[str]]:
     return re.search(
         r'\w{3}\s+\d{1,2}\s\d{2}:\d{2}:\d{2}\s\w+\ssshd\[\d+]:.*', line)
 
 
-def get_date(line):
+def get_date(line: str) -> str:
     return re.search(r'\w{3}\s+\d{1,2}\s\d{2}:\d{2}:\d{2}', line).group(0)
 
 
-def get_host_name(line):
+def get_host_name(line: str) -> str:
     return re.search(r'(?<=:\d{2} )\w+(?=\s)', line).group(0)
 
 
-def get_pid(line):
+def get_pid(line: str) -> str:
     return re.search(r'(?<=\[)\d+(?=])', line).group(0)
 
 
-def get_message(line):
+def get_message(line: str) -> str:
     return re.search(r'(?<=]:\s).+', line).group(0)
 
 
-def get_ipv4s_from_log(message):
+def get_ipv4s_from_log(message: str) -> list[str]:
     return re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', message)
 
 
-def get_user_from_log(message):
-    user = re.search(
+def get_user_from_log(message: str) -> Optional[str]:
+    user: Match[str] = re.search(
         r'((?<=(user\s(?!request)(?!authentication)))|(?<=((?<!getaddrinfo)(?<!Thank you)\sfor\s(?!invalid)))|(?<=(\suser=)))\S+',
         message)
 
@@ -38,8 +39,8 @@ def get_user_from_log(message):
         return None
 
 
-def get_port_from_log(message):
-    port = re.search(r'(?<=port\s)\d+', message)
+def get_port_from_log(message: str) -> Optional[str]:
+    port: Match[str] = re.search(r'(?<=port\s)\d+', message)
 
     if port:
         return port.group(0)
@@ -48,13 +49,13 @@ def get_port_from_log(message):
 
 
 class MessageType(Enum):
-    FAILED_PASSWORD = 'Failed password'
-    ACCEPTED_PASSWORD = 'Accepted password'
-    ERROR = 'Error'
-    OTHER = 'Other'
+    FAILED_PASSWORD: str = 'Failed password'
+    ACCEPTED_PASSWORD: str = 'Accepted password'
+    ERROR: str = 'Error'
+    OTHER: str = 'Other'
 
 
-def get_message_type(message):
+def get_message_type(message: str) -> MessageType:
     if re.search(r'^Failed password', message):
         return MessageType.FAILED_PASSWORD
 
