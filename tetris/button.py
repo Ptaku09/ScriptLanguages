76a3026fe3a7be77a168a5c25c1b5ab11a8bfee3
Mateusz Ptak <ptakm02@gmail.com>
2023-06-13 19:31:55 +0200

    tetris: create new button class

--- tetris/button.py
+++ tetris/button.py
@@ -1,35 +1,29 @@
-import pygame as py
-
-
 class Button:
-    def __init__(self, x, y, sx, sy, bg_color, font_bg_color, font_color, text):
-        self.x = x
-        self.y = y
-        self.sx = sx
-        self.sy = sy
-        self.font_size = 25
-        self.bg_color = bg_color
-        self.font_bg_color = font_bg_color
-        self.font_color = font_color
-        self.text = text
-        self.current_state = False
-        self.button_font = py.font.Font('./fonts/ubuntu.ttf', self.font_size)
+    def __init__(self, pos, text_input, font, base_color, hovering_color):
+        self.x_pos = pos[0]
+        self.y_pos = pos[1]
+        self.font = font
+        self.base_color, self.hovering_color = base_color, hovering_color
+        self.text_input = text_input
+        self.text = self.font.render(self.text_input, True, self.base_color)
+        self.image = self.text
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
 
-    def show_button(self, display):
-        if self.current_state:
-            py.draw.rect(display, self.font_bg_color, (self.x, self.y, self.sx, self.sy))
+    def update(self, screen):
+        if self.image is not None:
+            screen.blit(self.image, self.rect)
+        screen.blit(self.text, self.text_rect)
+
+    def check_for_input(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top,
+                                                                                          self.rect.bottom):
+            return True
+        return False
+
+    def change_color(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top,
+                                                                                          self.rect.bottom):
+            self.text = self.font.render(self.text_input, True, self.hovering_color)
         else:
-            py.draw.rect(display, self.bg_color, (self.x, self.y, self.sx, self.sy))
-
-        text_surface = self.button_font.render(self.text, False, self.font_color)
-        text_x = self.x + (self.sx / 2) - (self.font_size / 2) * (len(self.text) / 2) - 5
-        text_y = self.y + (self.sy / 2) - (self.font_size / 2) - 4
-        display.blit(text_surface, (text_x, text_y))
-
-    def focus_check(self, mouse_pos, mouseclick):
-        if self.x <= mouse_pos[0] <= self.x + self.sx and self.y <= mouse_pos[1] <= self.y + self.sy:
-            self.current_state = True
-            return mouseclick[0]
-        else:
-            self.current_state = False
-            return False
+            self.text = self.font.render(self.text_input, True, self.base_color)
